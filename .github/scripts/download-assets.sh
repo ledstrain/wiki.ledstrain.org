cd23c79bc0c367f9c809acacf131773197899c42
tyler71 <4926565+tyler71@users.noreply.github.com>
2020-01-08 15:05:52 -0800

    Update gh-pages.yml Update gh-pages.yml Create stats.yml Update stats.yml Update stats.yml Update stats.yml Update stats.yml Update stats.yml Update gh-pages.yml Update gh-pages.yml Delete stats.yml Create download-images.sh Update gh-pages.yml Update download-images.sh Update download-images.sh Update download-images.sh Update download-images.sh Update gh-pages.yml Rename download-images.sh to download-assets.sh Update gh-pages.yml

---
+++ .github/scripts/download-assets.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -n "${MATOMO_API}" ]
+then
+  apikey="${MATOMO_API}"
+else
+  echo No API Key Set
+  exit 1
+fi
+
+baseurl="https://track.xyzz.work/index.php?"
+
+mkdir -p public/docs/appendix/meta/stats/img
+wget ${baseurl}"module=API&method=ImageGraph.get&idSite=3&apiModule=UserCountry&apiAction=getCountry&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O public/docs/appendix/meta/stats/img/visits_country.png
+wget ${baseurl}"module=API&method=ImageGraph.get&idSite=3&apiModule=VisitsSummary&apiAction=get&token_auth=${apikey}&graphType=evolution&period=day&date=previous30&width=800&height=400" -O public/docs/appendix/meta/stats/img/visits_summary.png
+wget ${baseurl}"module=API&method=ImageGraph.get&idSite=3&apiModule=DevicesDetection&apiAction=getModel&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O public/docs/appendix/meta/stats/img/visits_device.png
