aa5d4477bea0f8bc7b8090871f6b01f617615efc
Unknown <sagsdf_github_tyler71@xyzz.work>
2020-01-08 17:22:42 -0800

    shellcheck script

--- .github/scripts/download-assets.sh
+++ .github/scripts/download-assets.sh
@@ -16,6 +16,6 @@ fi
 baseurl="https://track.xyzz.work/index.php?"
 
 mkdir -p "${basedir}"/docs/appendix/meta/stats/img
-wget ${baseurl}"module=API&method=ImageGraph.get&idSite=3&apiModule=UserCountry&apiAction=getCountry&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O ${basedir}/docs/appendix/meta/stats/img/visits_country.png
-wget ${baseurl}"module=API&method=ImageGraph.get&idSite=3&apiModule=VisitsSummary&apiAction=get&token_auth=${apikey}&graphType=evolution&period=day&date=previous30&width=800&height=400" -O ${basedir}/docs/appendix/meta/stats/img/visits_summary.png
-wget ${baseurl}"module=API&method=ImageGraph.get&idSite=3&apiModule=DevicesDetection&apiAction=getModel&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O ${basedir}/docs/appendix/meta/stats/img/visits_device.png
+wget "${baseurl}""module=API&method=ImageGraph.get&idSite=3&apiModule=UserCountry&apiAction=getCountry&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O "${basedir}"/docs/appendix/meta/stats/img/visits_country.png
+wget "${baseurl}""module=API&method=ImageGraph.get&idSite=3&apiModule=VisitsSummary&apiAction=get&token_auth=${apikey}&graphType=evolution&period=day&date=previous30&width=800&height=400" -O "${basedir}"/docs/appendix/meta/stats/img/visits_summary.png
+wget "${baseurl}""module=API&method=ImageGraph.get&idSite=3&apiModule=DevicesDetection&apiAction=getModel&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O "${basedir}"/docs/appendix/meta/stats/img/visits_device.png
