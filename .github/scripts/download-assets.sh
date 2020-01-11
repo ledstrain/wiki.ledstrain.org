943b258cf6a7a8952e3a20c8c697393a94e4078a
tyler71 <4926565+tyler71@users.noreply.github.com>
2020-01-10 17:31:06 -0800

    Update download-assets.sh

--- .github/scripts/download-assets.sh
+++ .github/scripts/download-assets.sh
@@ -13,16 +13,16 @@ elif [ "${BUILD}" == "COMPILED" ]; then
   basedir="public"
 fi
 
-baseurl="$MATOMO_BASEURL"
-
 mkdir -p "${basedir}"/docs/appendix/meta/stats/img
 
+stats_basedir="docs/appendix/meta/stats"
+
 # LEDStrain Forum
-wget "${baseurl}""module=API&method=ImageGraph.get&idSite=3&apiModule=VisitsSummary&apiAction=get&token_auth=${apikey}&graphType=evolution&period=day&date=previous30&width=800&height=400" -O "${basedir}"/docs/appendix/meta/stats/img/03_visits_summary.png
-wget "${baseurl}""module=API&method=ImageGraph.get&idSite=3&apiModule=UserCountry&apiAction=getCountry&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O "${basedir}"/docs/appendix/meta/stats/img/03_visits_country.png
-wget "${baseurl}""module=API&method=ImageGraph.get&idSite=3&apiModule=DevicesDetection&apiAction=getModel&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O "${basedir}"/docs/appendix/meta/stats/img/03_visits_device.png
+wget "${MATOMO_BASEURL}""module=API&method=ImageGraph.get&idSite=3&apiModule=VisitsSummary&apiAction=get&token_auth=${apikey}&graphType=evolution&period=day&date=previous30&width=800&height=400" -O "${basedir}"/"${stats_basedir}"/img/03_visits_summary.png
+wget "${MATOMO_BASEURL}""module=API&method=ImageGraph.get&idSite=3&apiModule=UserCountry&apiAction=getCountry&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O "${basedir}"/"${stats_basedir}"/img/03_visits_country.png
+wget "${MATOMO_BASEURL}""module=API&method=ImageGraph.get&idSite=3&apiModule=DevicesDetection&apiAction=getModel&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O "${basedir}"/"${stats_basedir}"/img/03_visits_device.png
 
 # LEDStrain Wiki
-wget "${baseurl}""module=API&method=ImageGraph.get&idSite=4&apiModule=VisitsSummary&apiAction=get&token_auth=${apikey}&graphType=evolution&period=day&date=previous30&width=800&height=400" -O "${basedir}"/docs/appendix/meta/stats/img/04_visits_summary.png
-wget "${baseurl}""module=API&method=ImageGraph.get&idSite=4&apiModule=UserCountry&apiAction=getCountry&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O "${basedir}"/docs/appendix/meta/stats/img/04_visits_country.png
-wget "${baseurl}""module=API&method=ImageGraph.get&idSite=4&apiModule=DevicesDetection&apiAction=getModel&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O "${basedir}"/docs/appendix/meta/stats/img/04_visits_device.png
+wget "${MATOMO_BASEURL}""module=API&method=ImageGraph.get&idSite=4&apiModule=VisitsSummary&apiAction=get&token_auth=${apikey}&graphType=evolution&period=day&date=previous30&width=800&height=400" -O "${basedir}"/"${stats_basedir}"/img/04_visits_summary.png
+wget "${MATOMO_BASEURL}""module=API&method=ImageGraph.get&idSite=4&apiModule=UserCountry&apiAction=getCountry&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O "${basedir}"/"${stats_basedir}"/img/04_visits_country.png
+wget "${MATOMO_BASEURL}""module=API&method=ImageGraph.get&idSite=4&apiModule=DevicesDetection&apiAction=getModel&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O "${basedir}"/"${stats_basedir}"/img/04_visits_device.png
