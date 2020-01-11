b2affa642e5b93f959a9b6953888f7739f1958fe
Unknown <sagsdf_github_tyler71@xyzz.work>
2020-01-10 17:53:15 -0800

    Simplify downloading of stats
    download to /resources

--- .github/scripts/download-assets.sh
+++ .github/scripts/download-assets.sh
@@ -7,22 +7,18 @@ else
   exit 1
 fi
 
-if [ "${BUILD}" == "SOURCE" ]; then
-  basedir="content"
-elif [ "${BUILD}" == "COMPILED" ]; then
-  basedir="public"
-fi
-
-mkdir -p "${basedir}"/docs/appendix/meta/stats/img
-
-stats_basedir="docs/appendix/meta/stats"
+#if [ "${BUILD}" == "SOURCE" ]; then
+#  basedir="content"
+#elif [ "${BUILD}" == "COMPILED" ]; then
+#  basedir="public"
+#fi
 
 # LEDStrain Forum
-wget "${MATOMO_BASEURL}""module=API&method=ImageGraph.get&idSite=3&apiModule=VisitsSummary&apiAction=get&token_auth=${apikey}&graphType=evolution&period=day&date=previous30&width=800&height=400" -O "${basedir}"/"${stats_basedir}"/img/03_visits_summary.png
-wget "${MATOMO_BASEURL}""module=API&method=ImageGraph.get&idSite=3&apiModule=UserCountry&apiAction=getCountry&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O "${basedir}"/"${stats_basedir}"/img/03_visits_country.png
-wget "${MATOMO_BASEURL}""module=API&method=ImageGraph.get&idSite=3&apiModule=DevicesDetection&apiAction=getModel&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O "${basedir}"/"${stats_basedir}"/img/03_visits_device.png
+wget "${MATOMO_BASEURL}""module=API&method=ImageGraph.get&idSite=3&apiModule=VisitsSummary&apiAction=get&token_auth=${apikey}&graphType=evolution&period=day&date=previous30&width=800&height=400" -O "resources/stats/img/03_visits_summary.png"
+wget "${MATOMO_BASEURL}""module=API&method=ImageGraph.get&idSite=3&apiModule=UserCountry&apiAction=getCountry&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O "resources/stats/img/03_visits_country.png"
+wget "${MATOMO_BASEURL}""module=API&method=ImageGraph.get&idSite=3&apiModule=DevicesDetection&apiAction=getModel&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O "resources/stats/img/03_visits_device.png"
 
 # LEDStrain Wiki
-wget "${MATOMO_BASEURL}""module=API&method=ImageGraph.get&idSite=4&apiModule=VisitsSummary&apiAction=get&token_auth=${apikey}&graphType=evolution&period=day&date=previous30&width=800&height=400" -O "${basedir}"/"${stats_basedir}"/img/04_visits_summary.png
-wget "${MATOMO_BASEURL}""module=API&method=ImageGraph.get&idSite=4&apiModule=UserCountry&apiAction=getCountry&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O "${basedir}"/"${stats_basedir}"/img/04_visits_country.png
-wget "${MATOMO_BASEURL}""module=API&method=ImageGraph.get&idSite=4&apiModule=DevicesDetection&apiAction=getModel&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O "${basedir}"/"${stats_basedir}"/img/04_visits_device.png
+wget "${MATOMO_BASEURL}""module=API&method=ImageGraph.get&idSite=4&apiModule=VisitsSummary&apiAction=get&token_auth=${apikey}&graphType=evolution&period=day&date=previous30&width=800&height=400" -O "resources/stats/img/04_visits_summary.png"
+wget "${MATOMO_BASEURL}""module=API&method=ImageGraph.get&idSite=4&apiModule=UserCountry&apiAction=getCountry&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O "resources/stats/img/04_visits_country.png"
+wget "${MATOMO_BASEURL}""module=API&method=ImageGraph.get&idSite=4&apiModule=DevicesDetection&apiAction=getModel&token_auth=${apikey}&graphType=horizontalBar&period=month&date=today&width=800&height=400" -O "resources/stats/img/04_visits_device.png"
--- content/docs/appendix/meta/stats/index.md
+++ content/docs/appendix/meta/stats/index.md
@@ -7,20 +7,20 @@ weight: 1
 ## LEDStrain
 ### Forum
 
-{{< resource-image "img/03_visits_summary*" "Visits Summary 30 days" >}}  
+{{< resource-image "/img/03_visits_summary*" "Visits Summary 30 days" >}}  
 
-{{< resource-image "img/03_visits_country*" "Visits by Country 30 days" >}}  
+{{< resource-image "/img/03_visits_country*" "Visits by Country 30 days" >}}  
 
-{{< resource-image "img/03_visits_device*" "Visits by Device 30 days" >}}  
+{{< resource-image "/img/03_visits_device*" "Visits by Device 30 days" >}}  
 
 
 ### Wiki
 
-{{< resource-image "img/04_visits_summary*" "Visits Summary 30 days" >}}  
+{{< resource-image "/img/04_visits_summary*" "Visits Summary 30 days" >}}  
 
-{{< resource-image "img/04_visits_country*" "Visits by Country 30 days" >}}  
+{{< resource-image "/img/04_visits_country*" "Visits by Country 30 days" >}}  
 
-{{< resource-image "img/04_visits_device*" "Visits by Device 30 days" >}}  
+{{< resource-image "/img/04_visits_device*" "Visits by Device 30 days" >}}  
 
 ## Additional Stats
 
