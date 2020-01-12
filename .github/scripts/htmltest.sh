6b429fe4ac38da3f13c8008d96a8f0b739d1d3ac
tyler71 <4926565+tyler71@users.noreply.github.com>
2020-01-12 11:27:34 -0800

    Update htmltest.sh

--- .github/scripts/htmltest.sh
+++ .github/scripts/htmltest.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-wget 'https://raw.githubusercontent.com/ledstrain/wiki.ledstrain.org/master/.github/config/htmltest.yml' -O .htmltest.yml
+testDir="${TMPDIR}"/htmltest
+configFile="${TMPDIR}"/.htmltest.yml
+
+hugo -d "${testDir}"
+
+wget 'https://raw.githubusercontent.com/ledstrain/wiki.ledstrain.org/master/.github/config/htmltest.yml' -O "${configFile}"
 curl https://htmltest.wjdp.uk | bash
 chmod +x './bin/htmltest'
-./bin/htmltest --conf .htmltest.yml --skip-external './linkcheck'
+./bin/htmltest --conf "${configFile}" --skip-external "${testDir}"
