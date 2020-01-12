4f63379fce72250d73e79e32b53883aca6187780
tyler71 <4926565+tyler71@users.noreply.github.com>
2020-01-12 14:04:28 -0800

    Update htmltest.sh

--- .github/scripts/htmltest.sh
+++ .github/scripts/htmltest.sh
@@ -5,7 +5,7 @@ configFile="${RUNNER_TMPDIR}"/.htmltest.yml
 
 hugo -d "${testDir}"
 
-wget 'https://raw.githubusercontent.com/ledstrain/wiki.ledstrain.org/master/.github/config/htmltest.yml' -O "${configFile}"
+wget --no-verbose 'https://raw.githubusercontent.com/ledstrain/wiki.ledstrain.org/master/.github/config/htmltest.yml' -O "${configFile}"
 curl https://htmltest.wjdp.uk | bash
 chmod +x './bin/htmltest'
 ./bin/htmltest --conf "${configFile}" --skip-external "${testDir}"
